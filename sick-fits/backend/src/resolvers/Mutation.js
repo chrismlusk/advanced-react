9bab4fdd8ca8e741400671c785eaf0a65954a060
Chris Lusk <chris.m.lusk@gmail.com>
2018-10-15 19:47:48 -0500

    finish module 3

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,22 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if user is logged in
 
-module.exports = mutations;
+    try {
+      const item = await ctx.db.mutation.createItem(
+        {
+          data: {
+            ...args
+          }
+        },
+        info
+      );
+
+      return item;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+};
+
+module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,26 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+
+  //
+  // 👻 👻 👻
+  //
+  // Hot tip! If your Yoga query is the exact same as the Prisma query, then
+  // you can use the `prisma-binding` module's `forwardTo` method. That means
+  // anytime someone requests a list of items, we'll forward it to the database
+  // to be handled. No need to rewrite the query resolver!
+  // ==========================================================================
+
+  items: forwardTo('db')
+
+  // async items(parent, args, ctx, info) {
+  //   try {
+  //     const items = await ctx.db.query.items();
+  //     return items;
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
+};
 
 module.exports = Query;
