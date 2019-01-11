3f0a5cfa53779c9fc8bea2af247318bfcf3d3436
Chris Lusk <chrismlusk@chrismlusk.lan>
2019-01-11 12:22:34 -0600

    downgrade graphql deps to solve schema issues

--- sick-fits/frontend/config.js
+++ sick-fits/frontend/config.js
@@ -1,3 +1,4 @@
 // Client side only. Don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const perPage = 4;
+export const PER_PAGE = 4;
+export const STRIPE_KEY = `pk_test_t4Qn94dmD57lavZuV8x86f0i`;
